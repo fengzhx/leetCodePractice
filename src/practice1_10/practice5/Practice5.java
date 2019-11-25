cf3e541a6020662342635a2ef5962aaba40ece3f
fengzhx <fengzhx@boloomo.com>
2019-11-25 19:20:37 +0800

    no message

--- src/practice1_10/practice5/Practice5.java
+++ src/practice1_10/practice5/Practice5.java
@@ -2,10 +2,43 @@ package practice1_10.practice5;
 
 
 public class Practice5 {
+    public static void main(String[] args) {
+        Solution1 solution = new Solution1();
+        System.out.println(solution.longestPalindrome("babad"));
+    }
 }
 
-class Solution {
+/**
+ * 暴力法（超出时间限制）
+ */
+class Solution1 {
     public String longestPalindrome(String s) {
-        return "";
+        char[] chars = s.toCharArray();
+        int maxLength = -1;
+        String result = "";
+        int charLength = chars.length;
+        for(int i = 0;i<charLength;i++){
+            for(int j = i + 1;j<charLength + 1;j++){
+                String temp = s.substring(i,j);
+                if(isPalindrom(temp)){
+                    if(temp.length() > maxLength){
+                        maxLength = temp.length();
+                        result = temp;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public Boolean isPalindrom(String s){
+        char[] chars = s.toCharArray();
+        int charLength = chars.length;
+        for (int i = 0;i<charLength/2;i++){
+            if(chars[i] != chars[charLength - 1 - i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ src/test/Test.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = "asd";
+        System.out.println(a.substring(0, 1));
+    }
+}
