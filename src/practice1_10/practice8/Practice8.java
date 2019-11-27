8c63ae0c827aa137b5809dbbabf08e169911859f
fengzhx <fengzhx@boloomo.com>
2019-11-27 16:15:17 +0800

    no message

---
+++ src/practice1_10/practice8/Practice8.java
@@ -0,0 +1,14 @@
+package practice1_10.practice8;
+
+public class Practice8 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.myAtoi("42"));
+    }
+}
+
+class Solution {
+    public int myAtoi(String str) {
+        return 0;
+    }
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         String a = "asd";
-        System.out.println(a.substring(0, 1));
+        System.out.println(a.substring(0, 10));
     }
 }
