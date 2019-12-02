23ba4e0b8d6ee399d3b845ad2023c43e9e999cee
fengzhx <fengzhx@boloomo.com>
2019-12-02 17:24:58 +0800

    no message

--- src/practice11_20/practice15/Practice15.java
+++ src/practice11_20/practice15/Practice15.java
@@ -1,17 +1,49 @@
 package practice11_20.practice15;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Practice15 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+        System.out.println(solution.threeSum(new int[]{0,0,0}));
     }
 }
 
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        return null;
+        Arrays.sort(nums);
+        List<List<Integer>> lists = new LinkedList<>();
+        int length = nums.length;
+        for(int i = 0;i<length-2;i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            if(nums[i] > 0){
+                break;
+            }
+            int fixed = nums[i];
+            int right = i + 1;
+            int left = length - 1;
+            while(right < left){
+                int temp = fixed + nums[right] + nums[left];
+                if (temp > 0 || (left < length-1 && nums[left + 1] == nums[left])) {
+                    left --;
+                }else if(temp < 0 || (right > i + 1 && nums[right - 1] == nums[right])){
+                    right ++;
+                }else{
+                    List<Integer> list = new LinkedList<>();
+                    list.add(fixed);
+                    list.add(nums[right]);
+                    list.add(nums[left]);
+                    left --;
+                    right ++;
+                    lists.add(list);
+                }
+            }
+        }
+        return lists;
     }
 }
---
+++ src/practice21_30/practice22/Practice22.java
@@ -0,0 +1,16 @@
+package practice21_30.practice22;
+
+import java.util.List;
+
+public class Practice22 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.generateParenthesis(1));
+    }
+}
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
