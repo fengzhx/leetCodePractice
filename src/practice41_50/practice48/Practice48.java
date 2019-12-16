d40a2a2d841b6b8619a0dba9c73205ef17872cd0
fengzhx <fengzhx@boloomo.com>
2019-12-16 10:50:18 +0800

    no message

--- src/practice41_50/practice48/Practice48.java
+++ src/practice41_50/practice48/Practice48.java
@@ -11,14 +11,22 @@ public class Practice48 {
     }
 }
 
+/**
+ * 90度旋转图像
+ * 1.可以先把图像转置 然后再把每行颠倒顺序
+ * 2.一层一层逐个旋转
+ */
 class Solution {
     public void rotate(int[][] matrix) {
-        int length = matrix.length;
-    }
-
-    public void swap(int[][] matrix,int f1,int f2,int f3,int f4){
-        int temp = matrix[f1][f2];
-        matrix[f1][f2] = matrix[f3][f4];
-        matrix[f3][f4] = temp;
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
     }
 }
---
+++ src/practice51_60/practice55/Practice55.java
@@ -0,0 +1,17 @@
+package practice51_60.practice55;
+
+public class Practice55 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.canJump(new int[]{2, 3, 1, 1, 4}));
+    }
+}
+
+
+class Solution {
+    public boolean canJump(int[] nums) {
+
+
+        return false;
+    }
+}
